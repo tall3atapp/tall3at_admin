249ce7935e301e76411b10005aa322b01fa7a94b
ahmad01213 <aebrahima830@gmail.com>
2025-09-08 09:57:52 +0300

    prod deployed of this branch

--- src/constants/config.js
+++ src/constants/config.js
@@ -40,7 +40,7 @@ export const GENDER_OPTIONS = [
 // API Configuration
 export const API_CONFIG = {
   // BASE_URL: 'https://webapi.tall3at.com',
-  // BASE_URL: 'https://9f1f1a23cfa5.ngrok-free.app',
+  // BASE_URL: 'https://devwebapi.tall3at.com',
   BASE_URL: 'http://localhost:5030',
   TIMEOUT: 10000,
 };
